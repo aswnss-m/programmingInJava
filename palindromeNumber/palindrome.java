6817e22049f1df57486d31e326b3c5df87de19e2
aswin lal <aswnss.m@gmail.com>
2021-11-24 10:37:50 +0530

    palindrome

--- palindromeNumber/palindrome.java
+++ palindromeNumber/palindrome.java
@@ -1,32 +1,35 @@
+
 //To get input
 import java.util.Scanner;
 
-public class palindrome{
-    //function to reverse the number
-    static int reverse(int num){
-        int rev=0;
-        while(num != 0){
-            rev = (rev*10) + (num%10);
-            num = num/10;
+public class palindrome {
+    // function to reverse the number
+    static int reverse(int num) {
+        int rev = 0;
+        while (num != 0) {
+            rev = (rev * 10) + (num % 10);
+            num = num / 10;
         }
-        //returning the reversed number
+        // returning the reversed number
         return rev;
     }
-    public static void main(String args[]){
-        int num,revnum;
+
+    public static void main(String args[]) {
+        int num, rev_num;
         Scanner Sr = new Scanner(System.in);
         System.out.print("Enter a number : ");
-        
-        //getting the input
+
+        // getting the input
         num = Sr.nextInt();
-        revnum = reverse(num); //function call
-        
-        System.out.println("Reversed : "+ revnum);
-        
-        //checking whether palindrome or not
-        if(revnum == num)
+        rev_num = reverse(num); // function call
+
+        System.out.println("Reversed : " + rev_num);
+
+        // checking whether palindrome or not
+        if (rev_num == num)
             System.out.println("Palindrome!");
         else
             System.out.println("Not Palindrome!");
+        Sr.close();
     }
 }
